57742bcebcd159418caf9eea6074cac28872df80
Danny <danny.costelloe@gmail.com>
2021-03-15 22:59:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(driverNames, string) {
+    return driverNames.filter( (driverName) => driverName.toUpperCase() == string.toUpperCase())
+};
+
+function fuzzyMatch(driverNames, string){
+    return driverNames.filter( (driverName) => driverName[0] == string[0]);
+};
+
+function matchName(driverObjects, string) {
+    return driverObjects.filter( (object) => {
+        return object.name == string
+    })
+}
